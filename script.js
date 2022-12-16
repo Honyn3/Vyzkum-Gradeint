5bbd00433c1ec0d54fc471f9c15dbc55d6a12372
Honyn3 <honza.krahula2003@gmail.com>
2022-12-16 09:55:28 +0100

    colors

--- script.js
+++ script.js
@@ -34,9 +34,10 @@ function MiddleButtonPressed() {
     btnIndex = 1;
     colorPicker.style.display = "inherit";
     backgroundClick = true;
+    MiddleColor = odpovedi[pocetstranek - 1][1];
 
     ChangeActive();
-
+    MoveGradient();
 }
 function DisableMiddleButton() {
     MiddleColor = "null";
