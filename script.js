f1b3c387b14927b987cb47eb1c52edc816e0a92c
AdamJezdec <a.zatloukal.19@student.gymzl.cz>
2022-12-18 00:11:28 +0100

    tutorial

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let leftButton = document.getElementById("colorButtonLeft");
 let gradient = document.getElementById("gradient");
 let middleButton = document.getElementById("colorButtonMiddle");
 let middleSwitch = document.getElementById("checkbox");
+let page = document.getElementById("stranka");
+let disableMiddleButton  = document.getElementById("disableMiddleButton");
 
 let btnIndex = 0;
 let colorWidthHalf = 95;
@@ -17,7 +19,6 @@ let backgroundClick = false;
 
 
 function hideColor() {
-
     ChangeActive();
 }
 function LeftButtonPressed() {
@@ -29,7 +30,6 @@ function LeftButtonPressed() {
 }
 function MiddleButtonPressed() {
     colorPicker.style.marginLeft = middleButton.getBoundingClientRect().x - colorWidthHalf + "px";
-    middleButton.style.backgroundColor = odpovedi[pocetstranek - 1][1];
     middleSwitch = true;
     btnIndex = 1;
     colorPicker.style.display = "inherit";
@@ -63,6 +63,12 @@ function backgroundClicked(){
 
     backgroundClick = false;
 }
+function backgroundClickedtutorial(){
+    colorPicker.style.display = "none";
+    btnIndex = -1;
+    ChangeActive();
+    backgroundClick = false;
+}
 function MoveToButtonIndex() {
     if (btnIndex == 0)
         LeftButtonPressed();
@@ -72,7 +78,6 @@ function MoveToButtonIndex() {
         RightButtonPressed();
 }
 function ChangeActive() {
-    console.log(btnIndex);
     switch (btnIndex) {
         case 0:
             leftButton.style.borderWidth = '4px';
@@ -150,6 +155,9 @@ function GradientAdjust(x) {
     y = 0.8 * (x - 50) + 50;
     return y;
 }
+
+
+
 
 let pocetstranek = 1;
 let soupisotazek = [["skvělé", "dobré", "neutrální", "špatné", "příšerné"], ["nádherné", "hezké", "neutrální", "ošklivé", "ohavné"], ["rychlé", "svižné", "neutrální", "zvolna", "pomalé"], ["biologie", "programování", "chemie", "němčina"]] //slova nad sliderem (budou v budoucnu přicházet ze serveru)
@@ -291,4 +299,229 @@ function zpatky() {
         }
     }
 
+}
+
+//tutorial 
+document.getElementById("cursortutorial").style.display="none";
+let tutorialwords = ["horké","vlažné","studené"];
+const tutorialtext = document.createElement("div");
+tutorialtext.setAttribute("id","tutorialtext");
+document.body.appendChild(tutorialtext);
+movesofcursor();
+function movesofcursor (){
+    document.getElementById("dalsi").style.display="none";
+    document.getElementById("zpatky").style.display="none";
+    document.getElementById("cislostranky").style.display="none";
+    document.getElementById("stranka").style.display = "block";
+    document.getElementById("buttonstart").style.display = "none";
+    document.getElementById("scaleLi").innerHTML = ''; //vypis slov nad sliderem
+        for (let i = 0; i < tutorialwords.length; i++) {
+            document.getElementById("scaleLi").innerHTML += "<li style='text-align:center;width:" + 100 / tutorialwords.length + "%'>" + tutorialwords[i] + "</li>";
+        }
+        page.onclick = "";
+        document.body.onclick = "";
+        for(let i = 0; i < document.getElementsByTagName("button").length; i++){
+            document.getElementsByTagName("button")[i].onclick="";
+        }
+        for(let i = 0; i < document.getElementsByTagName("input").length; i++){
+            document.getElementsByTagName("input")[i].onclick="";
+        }
+        DisableMiddleButton();
+        // create tutorial pop up
+        const tutorialrealtext = document.createTextNode("Budeme mít za úkol přiřadit barvy ke slovům nad nimi, můžete přiřadit 2 nebo 3 barvy jak uvidíte v následující animaci.");
+        tutorialtext.appendChild(tutorialrealtext);
+        const tutorialbutton = document.createElement("button");
+        const textnode = document.createTextNode("Další");
+        tutorialbutton.appendChild(textnode);
+        tutorialtext.appendChild(tutorialbutton);
+        tutorialbutton.setAttribute("id","tutorialbutton");
+        page.style.filter ="blur(8px)"
+        tutorialbutton.onclick= function (){
+        document.getElementById("cursortutorial").style.display="block";
+        page.style.filter ="blur(0px)"
+        setTimeout(firstmove, 500);
+        tutorialtext.style.display="none";
+        } 
+        
+    }
+function firstmove(){
+    document.querySelector(':root').style.setProperty('--secondleft', document.getElementById("colorButtonLeft").getBoundingClientRect().x.toFixed()+'px');
+    document.querySelector(':root').style.setProperty('--secondtop', document.getElementById("colorButtonLeft").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //first move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(secondmove, 2500);
+}
+function secondmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("button8").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("button8").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(thirdmove, 2500);
+}
+function thirdmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("colorButtonRight").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("colorButtonRight").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(fourthmove, 2500);
+}
+function fourthmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("button20").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("button20").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(fifthmove, 2500);
+}
+function fifthmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("colorButtonMiddle").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("colorButtonMiddle").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(sixthmove, 2500);
+}
+function sixthmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("button4").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("button4").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    while (tutorialtext.firstChild) {
+        tutorialtext.removeChild(tutorialtext.firstChild);
+    }
+    setTimeout(() => {
+    page.style.filter ="blur(0px)"
+    tutorialtext.style.display="block";
+    const tutorialrealtext = document.createTextNode("Prostřední barvu můžete též zrušit a nechat pouze dvě.");
+    tutorialtext.appendChild(tutorialrealtext);
+    const tutorialbutton = document.createElement("button");
+    const textnode = document.createTextNode("Další");
+    tutorialbutton.appendChild(textnode);
+    tutorialtext.appendChild(tutorialbutton);
+    tutorialbutton.setAttribute("id","tutorialbutton");
+    page.style.filter ="blur(8px)"
+    tutorialbutton.onclick= function (){
+    page.style.filter ="blur(0px)"
+    setTimeout(sevethmove, 500);
+    tutorialtext.style.display="none";
+    } 
+    }, 2500); 
+}
+function sevethmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("disableMiddleButton").getBoundingClientRect().x.toFixed()+'px');
+    r.style.setProperty('--secondtop', document.getElementById("disableMiddleButton").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    setTimeout(eighthmove,2500)
+}
+function eighthmove(){
+    document.getElementById("cursortutorial").style.animation = "none";
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    r.style.setProperty('--firstleft',rs.getPropertyValue('--secondleft'));
+    r.style.setProperty('--firsttop',rs.getPropertyValue('--secondtop'));
+    r.style.setProperty('--secondleft', document.getElementById("button7").getBoundingClientRect().x.toFixed()*1.2+'px');
+    r.style.setProperty('--secondtop', document.getElementById("button7").getBoundingClientRect().top.toFixed()+'px');
+    document.getElementById("cursortutorial").style.animation = "moves 2s forwards"; //second move of the cursor
+    setTimeout(tutorialclick, 2000);
+    while (tutorialtext.firstChild) {
+        tutorialtext.removeChild(tutorialtext.firstChild);
+    }
+    setTimeout(() => {
+    page.style.filter ="blur(0px)"
+    tutorialtext.style.display="block";
+    const tutorialrealtext = document.createTextNode("Nyní můžete pokračovat na výzkum.");
+    tutorialtext.appendChild(tutorialrealtext);
+    const tutorialbutton = document.createElement("button");
+    const textnode = document.createTextNode("Začít test");
+    tutorialbutton.appendChild(textnode);
+    tutorialtext.appendChild(tutorialbutton);
+    tutorialbutton.setAttribute("id","tutorialbutton");
+    page.style.filter ="blur(8px)"
+    tutorialbutton.onclick= function (){ //shows the start menu
+    for(let i = 0; i < document.getElementById("preset").getElementsByTagName("button").length; i++){
+            document.getElementById("preset").getElementsByTagName("button")[i].onclick=function (){Barvy(this);};
+    }
+    document.getElementById("dalsi").style.display="block";
+    document.getElementById("zpatky").style.display="block";
+    document.getElementById("cislostranky").style.display="block";
+    rightButton.onclick=function(){RightButtonPressed()}
+    leftButton.onclick=function(){LeftButtonPressed()}
+    middleButton.onclick=function(){MiddleButtonPressed()}
+    disableMiddleButton.onclick=function(){DisableMiddleButton()}
+    page.onclick=function(){hideColor()}
+    document.body.onclick=function(){backgroundClicked()}
+    document.getElementById("dalsi").onclick=function(){dalsi()}
+    document.getElementById("zpatky").onclick=function(){zpatky()}
+    page.style.filter ="blur(0px)"
+    setTimeout(start, 500);
+    tutorialtext.style.display="none";
+    document.getElementById("cursortutorial").style.display="none";
+    } 
+    }, 2500);
+}
+
+move=1;
+
+function tutorialclick(){
+    const ripple = document.createElement("div");
+   ripple.className = "ripple";
+   document.body.appendChild(ripple);
+  ripple.style.top =  document.getElementById("cursortutorial").getBoundingClientRect().top.toFixed()+"px";
+  ripple.style.left =   document.getElementById("cursortutorial").getBoundingClientRect().left.toFixed()+"px";
+   ripple.style.animation = "ripple-effect .4s  linear";
+   ripple.onanimationend = () => {
+     document.body.removeChild(ripple);
+   }
+   if(move==1){LeftButtonPressed();
+} else if(move==2){
+    
+    setTimeout(Barvy(document.getElementById("button8")), 1000);
+} else if(move==3){
+    
+    RightButtonPressed();
+} else if(move==4){
+    
+    setTimeout(Barvy(document.getElementById("button20")), 1000);
+} else if(move==5){
+    
+    MiddleButtonPressed();
+     // problem s automatickým přiřazením
+} else if(move==6){
+    
+    setTimeout(Barvy(document.getElementById("button4")), 1000);
+} else if(move==7){
+    DisableMiddleButton();
+} else if(move==8){
+    backgroundClickedtutorial();
+}
+   move+=1;
 }
